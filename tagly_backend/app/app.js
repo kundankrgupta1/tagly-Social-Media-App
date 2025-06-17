75d811a911a0619a23d60650e5423382338b3fd5
KUNDAN KUMAR GUPTA <kundankrgupta@outlook.com>
2025-06-17 11:43:57 +0530

    server

--- tagly_backend/app/app.js
+++ tagly_backend/app/app.js
@@ -5,8 +5,7 @@ import postRoutes from "../routes/post.route.js";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(cors({
-	// origin: "http://localhost:5173",
-	origin: "https://tagly-social-media-app.onrender.com",
+	origin: "http://localhost:5173",
 	credentials: true
 }));
 app.use(express.json());
